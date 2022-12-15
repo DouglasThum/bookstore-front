84c436570b30b78e43c0582337e6a92179c965f2
Douglas Oliveira <douglas.toliveira.dev@gmail.com>
2022-12-15 15:49:11 -0300

    Category delete - selecting category to be deleted by id

--- src/app/components/views/category/category-delete/category-delete.component.ts
+++ src/app/components/views/category/category-delete/category-delete.component.ts
@@ -1,4 +1,7 @@
+import { Category } from './../category.model';
+import { CategoryService } from './../category.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-category-delete',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryDeleteComponent implements OnInit {
 
-  constructor() { }
+  category: Category = {
+    id: '',
+    name: '',
+    description: ''
+  }
+
+  constructor(private service: CategoryService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.category.id = this.route.snapshot.paramMap.get('id')!
+    this.findById()
+  }
+
+  findById(): void {
+    this.service.findById(this.category.id!).subscribe((response) => {
+      this.category = response
+      console.log(this.category)
+    })
   }
 
 }
